3e8589144226ffbef847c4f870f778de24b42016
Olivier Dovelos <43583237+LeZizzi@users.noreply.github.com>
2019-12-07 15:22:07 +0000

    Create keys.cmd

---
+++ keys.cmd
@@ -0,0 +1,2 @@
+
+gcloud kms encrypt --plaintext-file=canvas-sentinel-216122-7a1f90559b64.json --ciphertext-file=canvas-sentinel-216122-7a1f90559b64.json.enc  --location=global  --keyring=rbuild   --key=rbuilder
