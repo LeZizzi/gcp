d3947db99ed7730c213ce5f76c79e1643d1de477
Olivier Dovelos <43583237+LeZizzi@users.noreply.github.com>
2019-12-07 15:34:15 +0000

    Update keys.sh

--- keys.sh
+++ keys.sh
@@ -1,17 +1,17 @@
 
 # Create a key ring where to hook your encrypted keys
-gcloud kms keyrings create rb1 --location=europe-west-1
+gcloud kms keyrings create rb1 --location=global
 
 # Encrypt the credentials of the cloud runner service account
-gcloud kms keys create rb1_runner_key --location europe-west-1 \
+gcloud kms keys create rb1_runner_key --location global \
   --keyring rb1 --purpose encryption
-gcloud kms encrypt - location europe-west-1 --keyring rb1 \
+gcloud kms encrypt - location global --keyring rb1 \
   --key rb1_runner_key --plaintext-file ./config/rb1_runner.key \
   --ciphertext-file ./config/rb1_runner.key.enc
 
 # Do the same with the Rails master key file
-gcloud kms keys create rails_master_key --location europe-west-1 \
+gcloud kms keys create rails_master_key --location global \
   --keyring photo-album --purpose encryption
-gcloud kms encrypt --location europe-west-1 --keyring rb1 \
+gcloud kms encrypt --location global --keyring rb1 \
   --key rails_master_key --plaintext-file ./config/master.key \
   --ciphertext-file ./config/master.key.enc
